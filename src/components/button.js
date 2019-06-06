40d8d8d9d52d1f2f1544b99228b4b6ba36059ac3
Cris <criston2011@gmail.com>
2019-06-06 21:20:41 +0530

    removed unwanted font styles

--- src/components/button.js
+++ src/components/button.js
@@ -8,35 +8,21 @@ const StyledButton = styled.button`
 	color: #fff;
 	color: transparent;
 	transition-duration: 0.3s;
-	background: linear-gradient(to right, #13f3b9 0%, #5878f3 50%);
-	background-clip: text;
 	padding: 10px 15px;
 	transition: all 0.3s ease-in;
-
-	box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-
-	font-size: ${props => {
-		if (props.big) return "20px"
-		return "16px"
-	}};
+	box-shadow: 0px 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+	font-size:16px;
 	outline: none;
 	border: none;
 	cursor: pointer;
 	margin: 15px;
-	border: 0px solid ${props => (props.secondary ? "#F7A072" : "#a1cdf1")};
-	${props => {
-		return (
-			props.inverse &&
-			css`
-				background-color: #fff;
-				color: #a1cdf1;
-			`
-		)
-	}}
+	background: linear-gradient(to right, #13f3b9 0%, #5878f3 50%);	
+	background-clip: text;
 	&:hover {
 		backgroundposition: 0 0;
 		outline: none;
-		box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
+		box-shadow: 0 8px 16px rgba(50, 50, 93, 0.1), 0 6px 12px rgba(0, 128, 255, 0.3);
+	
 	}
 `
 
