208e791101e2fa3fc1b685a5607097bbf7937d4b
Cris <criston2011@gmail.com>
2019-06-07 18:10:20 +0530

    gradient button

--- src/components/button.js
+++ src/components/button.js
@@ -6,23 +6,20 @@ const StyledButton = styled.button`
 	
 	background-color: ${props => (props.secondary ? "#F7A072" : "#fff")};
 
-	transition-duration: 0.3s;
-	padding: 10px 15px;
-	transition: all 0.3s ease-in;
-	box-shadow: 0px 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
 	font-size:16px;
 	outline: none;
 	border: none;
 	cursor: pointer;
 	height:45px;
 	border-radius: 20px;
-	background: -webkit-linear-gradient(to right, #13f3b9 0%, #5878f3 50%);	
-	background: -moz-linear-gradient(to right, #13f3b9 0%, #5878f3 50%);	
-
-	background-clip: text;
-	color: transparent;
-	-webkit-background-clip:"text" ;
-	color:transparent;
+	background: -webkit-linear-gradient(top right ,#13f3b9, #5878f3 );	
+	-webkit-background-clip:text;
+	-webkit-text-fill-color:transparent;
+	transition-duration: 0.3s;
+	padding: 10px 15px;
+	transition: all 0.3s ease-in;
+	box-shadow: 0px 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+	
 	&:hover {
 		backgroundposition: 0 0;
 		outline: none;
