290919060b077307ef0cb7046a35addc35e40fa7
Cris <criston2011@gmail.com>
2019-06-08 21:52:32 +0530

    Now its a PWA 🤩

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Glug Infinite`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `FOSS Club`,
+    author: `@github:cristonkrizz`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
