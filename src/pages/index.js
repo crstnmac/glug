4cb6e5660d9d252321c4000afe51c0e3da530815
Cris <criston2011@gmail.com>
2020-04-27 13:39:36 +0530

    Updated SEO config

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,27 +4,31 @@ import Top from "./Home/top"
 import Middle from "./Home/middle"
 import Bottom from "./Home/bottom"
 import { Box } from "rebass"
+import SEO from "../components/seo"
 
 function IndexPage() {
- return( <Layout>
-    <Box
-      height="100%"
-      width={["100%", "80%", "80%"]}
-      marginLeft="auto"
-      marginRight="auto"
-      color="#2d3748"
-      fontFamily="Varela Round, sans-serif"
-      display="flex"
-      flexDirection="row"
-      flexWrap="wrap"
-      flexFlow="column"
-      justifyContent="space-between"
-    >
-      <Top />
-      <Middle />
-      <Bottom />
-    </Box>
-  </Layout>)
+  return (
+    <Layout>
+      <SEO title="Glug Infinite" description="Official glug-infinite website" />
+      <Box
+        height="100%"
+        width={["100%", "80%", "80%"]}
+        marginLeft="auto"
+        marginRight="auto"
+        color="#2d3748"
+        fontFamily="Varela Round, sans-serif"
+        display="flex"
+        flexDirection="row"
+        flexWrap="wrap"
+        flexFlow="column"
+        justifyContent="space-between"
+      >
+        <Top />
+        <Middle />
+        <Bottom />
+      </Box>
+    </Layout>
+  )
 }
 
 export default IndexPage
